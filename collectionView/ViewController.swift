846a31affb40f4381b1a71abd547834cf93aa6e4
MonaMouawadDafagate <mona.mouawad@dafagate.com>
2025-02-16 14:03:30 +0200

    Initial Commit

---
+++ collectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collectionView
+//
+//  Created by Mona on 16/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
