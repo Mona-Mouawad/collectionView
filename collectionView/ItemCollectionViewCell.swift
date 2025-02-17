82fac03ca237bb8e78142d74c6e83b47689e047a
MonaMouawadDafagate <mona.mouawad@dafagate.com>
2025-02-17 17:22:15 +0200

    UICollectionView

---
+++ collectionView/ItemCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ItemCollectionViewCell.swift
+//  collectionView
+//
+//  Created by Mona on 17/02/2025.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ collectionView/productEntity.swift
@@ -0,0 +1,8 @@
+//
+//  productEntity.swift
+//  collectionView
+//
+//  Created by Mona on 17/02/2025.
+//
+
+import Foundation
